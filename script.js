acade99d7f35dc769ae095136c0e60ece73a1c6e
Thamires Vasconcelos <thamiresvasconcelos23@gmail.com>
2023-10-19 21:14:07 -0300

    Animação

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function toggleMode(){
 
   const img = document.querySelector("#profile img");
 
-  if(html.classList.contains('light')){
-    img.setAttribute('src', './assets/avatar-light.png')
+  if(html.classList.contains("light")){
+    img.setAttribute("src", "./assets/avatar-light.png")
   }
   else{
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute("src", "./assets/avatar.png")
 
   }
 }
